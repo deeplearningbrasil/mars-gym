a097bbcc148be9564ed39cd805b7e4d98dbabf17
Fernando Camargo <fernando.camargo.ti@gmail.com>
2019-06-19 12:04:48 -0300

    - MatrixFactorization foi refatorada.

--- recommendation/model/matrix_factorization.py
+++ recommendation/model/matrix_factorization.py
@@ -11,21 +11,17 @@ class MatrixFactorization(nn.Module):
     def __init__(self, n_users: int, n_items: int, n_factors: int = 20,
                  weight_init: Callable = lecun_normal_init):
         super().__init__()
-        self.user_factors = nn.Embedding(n_users, n_factors)
-        self.item_factors = nn.Embedding(n_items, n_factors)
+        self.user_embeddings = nn.Embedding(n_users, n_factors)
+        self.item_embeddings = nn.Embedding(n_items, n_factors)
 
-        self.weight_init = weight_init
-        self.apply(self.init_weights)
-
-    def init_weights(self, module: nn.Module):
-        if type(module) == nn.Embedding:
-            self.weight_init(module.weight)
+        weight_init(self.user_embeddings.weight)
+        weight_init(self.item_embeddings.weight)
 
     def forward(self, user_item_tuple: torch.Tensor) -> torch.Tensor:
-        user: torch.Tensor = user_item_tuple[:, 0].to(torch.int64)
-        item: torch.Tensor = user_item_tuple[:, 1].to(torch.int64)
+        user_ids: torch.Tensor = user_item_tuple[:, 0].to(torch.int64)
+        item_ids: torch.Tensor = user_item_tuple[:, 1].to(torch.int64)
 
-        return (self.user_factors(user) * self.item_factors(item)).sum(1)
+        return (self.user_factors(user_ids) * self.item_factors(item_ids)).sum(1)
 
 
 class BiasedMatrixFactorization(nn.Module):
@@ -33,22 +29,27 @@ class BiasedMatrixFactorization(nn.Module):
     def __init__(self, n_users: int, n_items: int, n_factors: int = 20,
                  weight_init: Callable = lecun_normal_init):
         super().__init__()
-        self.user_factors = nn.Embedding(n_users, n_factors)
-        self.item_factors = nn.Embedding(n_items, n_factors)
-        self.user_biases = nn.Embedding(n_users, 1)
-        self.item_biases = nn.Embedding(n_items, 1)
+        self.user_embeddings = nn.Embedding(n_users, n_factors)
+        self.item_embeddings = nn.Embedding(n_items, n_factors)
+        self.user_bias = nn.Embedding(n_users, 1)
+        self.item_bias = nn.Embedding(n_items, 1)
 
-        self.weight_init = weight_init
-        self.apply(self.init_weights)
+        weight_init(self.user_embeddings.weight)
+        weight_init(self.item_embeddings.weight)
 
-    def init_weights(self, module: nn.Module):
-        if type(module) == nn.Embedding:
-            self.weight_init(module.weight)
+        self.user_bias.weight.data.zero_()
+        self.item_bias.weight.data.zero_()
 
     def forward(self, user_item_tuple: torch.Tensor) -> torch.Tensor:
-        user: torch.Tensor = user_item_tuple[:, 0].to(torch.int64)
-        item: torch.Tensor = user_item_tuple[:, 1].to(torch.int64)
+        user_ids: torch.Tensor = user_item_tuple[:, 0].to(torch.int64)
+        item_ids: torch.Tensor = user_item_tuple[:, 1].to(torch.int64)
 
-        pred: torch.Tensor = self.user_biases(user)[:, 0] + self.item_biases(item)[:, 0]
-        pred += (self.user_factors(user) * self.item_factors(item)).sum(1)
-        return pred
+        user_embedding = self.user_embeddings(user_ids)
+        item_embedding = self.item_embeddings(item_ids)
+
+        user_bias = self.user_bias(user_ids).squeeze(1)
+        item_bias = self.item_bias(item_ids).squeeze(1)
+
+        dot = (user_embedding * item_embedding).sum(1)
+
+        return dot + user_bias + item_bias
