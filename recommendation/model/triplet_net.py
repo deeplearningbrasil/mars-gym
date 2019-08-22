2b0fef8db1e21533ce7857adc5f15ec62cbe6351
Fernando Camargo <fernando.camargo.ti@gmail.com>
2019-08-22 12:50:49 -0300

    Corrigi o forward() do triplet.

--- recommendation/model/triplet_net.py
+++ recommendation/model/triplet_net.py
@@ -2,19 +2,19 @@ from typing import Tuple, Callable, Union
 
 import torch
 
-from recommendation.model.matrix_factorization import MatrixFactorization
+from recommendation.model.embedding import UserAndItemEmbedding
 from recommendation.utils import lecun_normal_init
 
 
-class TripletNet(MatrixFactorization):
+class TripletNet(UserAndItemEmbedding):
 
     def __init__(self, n_users: int, n_items: int, n_factors: int,
                  weight_init: Callable = lecun_normal_init):
-        super().__init__(n_users, n_items, n_factors, False, weight_init)
+        super().__init__(n_users, n_items, n_factors, weight_init)
 
     def forward(self, user_ids: torch.Tensor, positive_item_ids: torch.Tensor,
                 negative_item_ids: torch.Tensor = None) -> Union[Tuple[torch.Tensor, torch.Tensor, torch.Tensor], torch.Tensor]:
         if negative_item_ids is None:
-            return super().forward(user_ids, positive_item_ids)
+            return torch.cosine_similarity(self.user_embeddings(user_ids), self.item_embeddings(positive_item_ids))
         return self.user_embeddings(user_ids), self.item_embeddings(positive_item_ids), \
                self.item_embeddings(negative_item_ids)
