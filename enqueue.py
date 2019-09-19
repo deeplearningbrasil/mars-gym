3c41f406f7142d95eaeaeb33ce4fdcb06d40676d
luckeciano <luckeciano@gmail.com>
2019-09-19 19:20:55 -0300

    Adding enqueue CVAE

---
+++ enqueue.py
@@ -0,0 +1,87 @@
+from typing import List
+
+import luigi
+import numpy as np
+
+# - Para múltiplas GPUs:
+## ./cuda_luigid --background --pidfile /tmp/luigid.pid --logdir /tmp/luigid_log
+## PYTHONPATH="." ./cuda_luigi --module enqueue_pneumonia EnqueuePneumonia --seed 42 --workers 4
+# - Para uma única GPU:
+## PYTHONPATH="." luigi --module enqueue_pneumonia EnqueuePneumonia --seed 42 --local-scheduler
+from recommendation.task.ifood import EvaluateIfoodCDAEModel
+from recommendation.task.model.auto_encoder import UnconstrainedAutoEncoderTraining, AttentiveVariationalAutoEncoderTraining
+
+
+class EnqueueEvaluateCDAE(luigi.WrapperTask):
+    task_ids: List[str] = luigi.ListParameter()
+
+    def requires(self):
+        for task_id in self.task_ids:
+            yield EvaluateIfoodCDAEModel(
+                model_module="recommendation.task.model.auto_encoder",
+                model_cls="UnconstrainedAutoEncoderTraining",
+                model_task_id=task_id,
+            )
+
+class EnqueueCDAE(luigi.WrapperTask):
+    seed: int = luigi.IntParameter(default=42)
+    batch_size_options: list = luigi.ListParameter(default=[50, 100, 200, 500, 1000])
+    learning_rate_range: tuple = luigi.TupleParameter(default=(1e-5, 1e-3))
+
+    def requires(self):
+        random_state = np.random.RandomState(self.seed)
+
+        for i in range(5):
+            training_model = UnconstrainedAutoEncoderTraining(
+                project="ifood_user_cdae",
+                binary=True,
+                optimizer="adam",
+                learning_rate=random_state.uniform(self.learning_rate_range[0], self.learning_rate_range[1]),
+                batch_size=int(random_state.choice(self.batch_size_options)),
+                generator_workers=0,
+                loss_function="focal",
+                loss_function_params={"gamma": 10.0, "alpha": 1616.0},
+                loss_wrapper="none",
+                data_frames_preparation_extra_params={"split_per_user": True},
+                gradient_norm_clipping=2.0,
+                gradient_norm_clipping_type=1,
+                data_transformation="support_based",
+            )
+            yield training_model
+
+class EnqueueAttentiveCVAE(luigi.WrapperTask):
+    seed: int = luigi.IntParameter(default=42)
+    batch_size_options: list = luigi.ListParameter(default=[50, 100, 200, 500, 1000])
+    learning_rate_range: tuple = luigi.TupleParameter(default=(1e-5, 1e-3))
+    activation_function_options: list = luigi.ListParameter(default=["selu", "relu", "tanh", "linear", "sigmoid"])
+    vae_anneal_range: tuple = luigi.TupleParameter(default=(0.0, 1.0))
+    attention_anneal_range: tuple = luigi.TupleParameter(default=(0.0, 1.0))
+    dropout_prob_range: tuple = luigi.TupleParameter(default=(0.0, 0.9))
+    optimizer_options: list = luigi.ListParameter(default=["adam", "rmsprop", "sgd", "adadelta", "adagrad", "adamax"])
+    data_transformation_options: list = luigi.ListParameter(default=["support_based", "salt_and_pepper_noise", "none", "masking_noise"])
+
+    def requires(self):
+         
+        random_state = np.random.RandomState(self.seed)
+
+        for i in range(60):
+            training_model = AttentiveVariationalAutoEncoderTraining(
+                project="ifood_user_cdae",
+                binary=False,
+                optimizer=str(random_state.choice(self.optimizer_options)),
+                learning_rate=random_state.uniform(self.learning_rate_range[0], self.learning_rate_range[1]),
+                batch_size=int(random_state.choice(self.batch_size_options)),
+                generator_workers=0,
+                loss_function="attentive_vae_loss",
+                loss_function_params={"anneal": random_state.uniform(self.vae_anneal_range[0], self.vae_anneal_range[1]),\
+                 "anneal_att": random_state.uniform(self.attention_anneal_range[0], self.attention_anneal_range[1])},
+                data_frames_preparation_extra_params={"split_per_user": True},
+                data_transformation=str(random_state.choice(self.data_transformation_options)),
+                activation_function=str(random_state.choice(self.activation_function_options)),
+                dropout_module="dropout",
+                dropout_prob=random_state.uniform(self.dropout_prob_range[0], self.dropout_prob_range[1]),
+                encoder_layers=[600,200],
+                decoder_layers=[200],
+                attention_layers=[600],
+            )
+            yield training_model
