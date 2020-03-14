23273f76401cc55e7519f9cc6f4375116f1c273c
Fernando Camargo <fernando.camargo.ti@gmail.com>
2020-03-14 09:16:43 -0300

    Added action_space and observation_space to RecSysEnv.

--- recommendation/gym/envs/recsys.py
+++ recommendation/gym/envs/recsys.py
@@ -1,25 +1,41 @@
 from collections import OrderedDict
-from typing import Tuple, List
+from typing import Tuple, List, Any
 
 import gym
 import numpy as np
 import pandas as pd
 from gym import utils
+from gym import spaces
 
 
 class RecSysEnv(gym.Env, utils.EzPickle):
-    metadata = {'render.modes': ['human']}
+    metadata = {'render.modes': []}
+    reward_range = [0.0, 1.0]
 
     def __init__(self, dataset: pd.DataFrame, item_column: str):
+        super().__init__()
         self._dataset = dataset
         self._item_column = item_column
         self._obs_dataset: List[dict] = dataset.drop(columns=[item_column]).to_dict('records')
 
+        self.action_space = spaces.Discrete(self._dataset[item_column].max()+1)
+        self.observation_space = spaces.Dict({
+            key: self._convert_value_to_space(key, value)
+            for key, value in self._obs_dataset[0].items()
+        })
         self._current_index = 0
-        self.reward_range = [0.0, 1.0]
-        # TODO
-        # self.observation_space
-        # self.action_space
+
+    def _convert_value_to_space(self, key: str, value: Any) -> spaces.Space:
+        if isinstance(value, int):
+            return spaces.Discrete(self._dataset[key].max() + 1)
+        elif isinstance(value, float):
+            return spaces.Box(self._dataset[key].min(), self._dataset[key].max(), shape=(1,))
+        elif isinstance(value, np.ndarray):
+            if issubclass(value.dtype, np.integer):
+                return spaces.MultiDiscrete([self._dataset[key].max() + 1] * len(value))
+            elif issubclass(value.dtype, np.floating):
+                return spaces.Box(self._dataset[key].min(), self._dataset[key].max(), shape=value.shape)
+        raise ValueError("Unkown type in the observation space for {}:{}".format(key, value))
 
     def _compute_stats(self, action: float) -> dict:
         # TODO: Choose which batch metrics to return
