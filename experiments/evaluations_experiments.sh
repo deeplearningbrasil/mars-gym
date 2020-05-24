2b625863143e88791d0862ce5b9a653e8006c815
Marlesson <marlessonsa@gmail.com>
2020-05-24 01:55:26 -0300

    results

---
+++ experiments/evaluations_experiments.sh
@@ -0,0 +1,24 @@
+# "per_pos_item_idx",
+# "per_item",
+# "per_item_in_first_pos",
+# "per_item_given_pos",
+# "per_logistic_regression_of_pos_item_idx_and_item",
+# "per_logistic_regression_of_pos_item_idx_and_item_ps",
+
+
+PYTHONPATH="." luigi --module recommendation.task.model.trivago.evaluation EvaluateTrivagoTestSetPredictions --model-module recommendation.task.model.trivago.trivago_logistic_model --model-cls TrivagoLogisticModelInteraction --model-task-id TrivagoLogisticModelInteraction_selu____epsilon_greedy_6a1d38dd73 --fairness-columns "[\"device_idx\", \"platform_idx\"]" --local-scheduler --direct-estimator-module recommendation.task.model.trivago.trivago_logistic_model --direct-estimator-cls TrivagoLogisticModelTraining --fill-ps-strategy per_logistic_regression_of_pos_item_idx_and_item_ps
+
+
+PYTHONPATH="." luigi --module recommendation.task.model.evaluation EvaluateTestSetPredictions --model-module recommendation.task.model.trivago.trivago_logistic_model --model-cls TrivagoLogisticModelInteraction --model-task-id TrivagoLogisticModelInteraction_selu____softmax_explorer_acf090f664 --fairness-columns "[\"device_idx\", \"platform_idx\"]" --local-scheduler --direct-estimator-module recommendation.task.model.trivago.trivago_logistic_model --direct-estimator-cls TrivagoLogisticModelTraining
+
+PYTHONPATH="." luigi --module recommendation.task.model.trivago.evaluation EvaluateTrivagoTestSetPredictions --model-module recommendation.task.model.trivago.trivago_logistic_model --model-cls TrivagoLogisticModelInteraction --model-task-id TrivagoLogisticModelInteraction_selu____softmax_explorer_acf090f664 --fairness-columns "[\"device_idx\", \"platform_idx\"]" --local-scheduler --direct-estimator-module recommendation.task.model.trivago.trivago_logistic_model --direct-estimator-cls TrivagoLogisticModelTraining --fill-ps-strategy per_pos_item_idx
+
+PYTHONPATH="." luigi --module recommendation.task.model.trivago.evaluation EvaluateTrivagoTestSetPredictions --model-module recommendation.task.model.trivago.trivago_logistic_model --model-cls TrivagoLogisticModelInteraction --model-task-id TrivagoLogisticModelInteraction_selu____softmax_explorer_acf090f664 --fairness-columns "[\"device_idx\", \"platform_idx\"]" --local-scheduler --direct-estimator-module recommendation.task.model.trivago.trivago_logistic_model --direct-estimator-cls TrivagoLogisticModelTraining --fill-ps-strategy per_item
+
+PYTHONPATH="." luigi --module recommendation.task.model.trivago.evaluation EvaluateTrivagoTestSetPredictions --model-module recommendation.task.model.trivago.trivago_logistic_model --model-cls TrivagoLogisticModelInteraction --model-task-id TrivagoLogisticModelInteraction_selu____softmax_explorer_acf090f664 --fairness-columns "[\"device_idx\", \"platform_idx\"]" --local-scheduler --direct-estimator-module recommendation.task.model.trivago.trivago_logistic_model --direct-estimator-cls TrivagoLogisticModelTraining --fill-ps-strategy per_item_in_first_pos
+
+PYTHONPATH="." luigi --module recommendation.task.model.trivago.evaluation EvaluateTrivagoTestSetPredictions --model-module recommendation.task.model.trivago.trivago_logistic_model --model-cls TrivagoLogisticModelInteraction --model-task-id TrivagoLogisticModelInteraction_selu____softmax_explorer_acf090f664 --fairness-columns "[\"device_idx\", \"platform_idx\"]" --local-scheduler --direct-estimator-module recommendation.task.model.trivago.trivago_logistic_model --direct-estimator-cls TrivagoLogisticModelTraining --fill-ps-strategy per_item_given_pos
+
+PYTHONPATH="." luigi --module recommendation.task.model.trivago.evaluation EvaluateTrivagoTestSetPredictions --model-module recommendation.task.model.trivago.trivago_logistic_model --model-cls TrivagoLogisticModelInteraction --model-task-id TrivagoLogisticModelInteraction_selu____softmax_explorer_acf090f664 --fairness-columns "[\"device_idx\", \"platform_idx\"]" --local-scheduler --direct-estimator-module recommendation.task.model.trivago.trivago_logistic_model --direct-estimator-cls TrivagoLogisticModelTraining --fill-ps-strategy per_logistic_regression_of_pos_item_idx_and_item
+
+PYTHONPATH="." luigi --module recommendation.task.model.trivago.evaluation EvaluateTrivagoTestSetPredictions --model-module recommendation.task.model.trivago.trivago_logistic_model --model-cls TrivagoLogisticModelInteraction --model-task-id TrivagoLogisticModelInteraction_selu____softmax_explorer_acf090f664 --fairness-columns "[\"device_idx\", \"platform_idx\"]" --local-scheduler --direct-estimator-module recommendation.task.model.trivago.trivago_logistic_model --direct-estimator-cls TrivagoLogisticModelTraining --fill-ps-strategy per_logistic_regression_of_pos_item_idx_and_item_ps
---
+++ mab_run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+# PYTHONPATH="." luigi --module recommendation.task.deployment.matrix_factorization PackMatrixFactorization \
+# --model-module=recommendation.task.model.matrix_factorization --model-cls=MatrixFactorizationTraining \
+# --model-task-id='MatrixFactorizationTraining____500_False_9fce69a6de' 
+
+# PYTHONPATH="." luigi --module recommendation.task.deployment.auto_encoder PackAutoEncoder --model-cls VariationalAutoEncoderTraining --model-task-id 'VariationalAutoEncoderTraining_selu____500_2d11e525f7' --user-column account_id --user-idx-column account_idx --item-column buys_per_merchant_id --item-idx-column buys_per_merchant_idx --deploy-module cvae
+
+# PYTHONPATH="." luigi --module recommendation.task.deployment.auto_encoder PackAutoEncoder --model-cls UnconstrainedAutoEncoderTraining  --model-task-id 'UnconstrainedAutoEncoderTraining_selu____100_43e5570ed9' --user-column account_id --user-idx-column account_idx --item-column buys_per_merchant_id --item-idx-column buys_per_merchant_idx --deploy-module cdae
+
+
+# bentoml serve RandomRankingRecommender:latest --port 5000
+# bentoml serve MostPopularRankingRecommender:latest --port 5001
+# bentoml serve MostPopularPerUserRankingRecommender:latest --port 5002
+# bentoml serve MatrixFactorizationRankingRecommender:latest --port 5003
+# bentoml serve CVAERankingRecommender:latest --port 5004
+# bentoml serve CDAERankingRecommender:latest --port 5005
+
+
+# outro
+
+
+PYTHONPATH="." luigi --module recommendation.task.model.ensamble_mab EnsambleMABInteraction --project ifood_ensamble_mab --bandit-policy remote --bandit-policy-params '{"endpoints": ["http://localhost:5000/rank"]}' --obs-batch-size 1 --val-size 0 --obs "Random"
+
+PYTHONPATH="." luigi --module recommendation.task.model.ensamble_mab EnsambleMABInteraction --project ifood_ensamble_mab --bandit-policy remote --bandit-policy-params '{"endpoints": ["http://localhost:5001/rank"]}' --obs-batch-size 1 --val-size 0 --obs "MostPopular"
+
+PYTHONPATH="." luigi --module recommendation.task.model.ensamble_mab EnsambleMABInteraction --project ifood_ensamble_mab --bandit-policy remote --bandit-policy-params '{"endpoints": ["http://localhost:5002/rank"]}' --obs-batch-size 1 --val-size 0 --obs "MostPopularPerUser"
+
+PYTHONPATH="." luigi --module recommendation.task.model.ensamble_mab EnsambleMABInteraction --project ifood_ensamble_mab --bandit-policy remote --bandit-policy-params '{"endpoints": ["http://localhost:5003/rank"]}' --obs-batch-size 1 --val-size 0 --obs "MatrixFactorization"
+
+PYTHONPATH="." luigi --module recommendation.task.model.ensamble_mab EnsambleMABInteraction --project ifood_ensamble_mab --bandit-policy remote --bandit-policy-params '{"endpoints": ["http://localhost:5004/rank"]}' --obs-batch-size 1 --val-size 0 --obs "CVAE"
+
+PYTHONPATH="." luigi --module recommendation.task.model.ensamble_mab EnsambleMABInteraction --project ifood_ensamble_mab --bandit-policy remote --bandit-policy-params '{"endpoints": ["http://localhost:5005/rank"]}' --obs-batch-size 1 --val-size 0 --obs "CDAE"
+
+
+for i in $(seq 1 10) 
+do
+
+PYTHONPATH="." luigi --module recommendation.task.model.ensamble_mab EnsambleMABInteraction --project ifood_ensamble_mab --bandit-policy remote_epsilon_greedy --bandit-policy-params '{"endpoints": ["http://localhost:5001/rank",  "http://localhost:5003/rank", "http://localhost:5004/rank", "http://localhost:5005/rank"], "window_reward": 500}' --obs-batch-size 1 --val-size 0 --obs "[MAB] e-greedy $i" --seed $i
+
+
+PYTHONPATH="." luigi --module recommendation.task.model.ensamble_mab EnsambleMABInteraction --project ifood_ensamble_mab --bandit-policy remote_ucb --bandit-policy-params '{"endpoints": ["http://localhost:5001/rank",  "http://localhost:5003/rank", "http://localhost:5004/rank", "http://localhost:5005/rank"]}' --obs-batch-size 1 --val-size 0 --obs "[MAB] UCB  $i" --seed $i
+
+
+PYTHONPATH="." luigi --module recommendation.task.model.ensamble_mab EnsambleMABInteraction --project ifood_ensamble_mab --bandit-policy remote_contextual_epsilon_greedy --bandit-policy-params '{"endpoints": ["http://localhost:5001/rank",  "http://localhost:5003/rank", "http://localhost:5004/rank", "http://localhost:5005/rank"]}' --obs-batch-size 1 --val-size 0 --obs "[Contextual] e-greedy $i" --seed $i
+
+PYTHONPATH="." luigi --module recommendation.task.model.ensamble_mab EnsambleMABInteraction --project ifood_ensamble_mab --bandit-policy remote_contextual_softmax --bandit-policy-params '{"endpoints": ["http://localhost:5001/rank", "http://localhost:5003/rank", "http://localhost:5004/rank", "http://localhost:5005/rank"]}' --obs-batch-size 1 --val-size 0 --obs "[Contextual] Softmax $i" --seed $i
+
+done
